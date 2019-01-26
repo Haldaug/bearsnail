2ee270dff7fb61f4b41026e47b8d3b1d8916637a
Petter Amland poketop <pokepetter@gmail.com>
2019-01-26 19:37:05 +0100

    made a level.

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public float rotationSpeed = 10;
+
+    private void Update() {
+        transform.Rotate(-Vector3.forward * Time.deltaTime * rotationSpeed);
+    }
+
+    private void OnTriggerEnter2D() {
+        Destroy(gameObject);
+
+    }
+}
